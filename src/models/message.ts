1fc99aed1a25ca7f95e708a57d413f40df64cde1
Nesim Tunc <nesimtunc@gmail.com>
2024-01-03 17:00:09 +0900

    Extend Message model for encryption

--- src/models/message.ts
+++ src/models/message.ts
@@ -1,6 +1,8 @@
-export interface Message {  
+export interface Message {
   id: string;
   content: string;
+  salt: string | undefined;
+  iv: string | undefined;
   isPrivate: boolean;
   viewCount: number;
   createdAt: Date;
