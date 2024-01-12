5862ec87fb9ccdcacd881558d53e9cc1bee2f2cc
Nesim Tunc <nesimtunc@gmail.com>
2024-01-12 13:10:36 +0900

    Update Jest configuration and package.json
    This commit updates the Jest configuration in jest.config.ts to use the root directory for test matching. It also modifies the test script in package.json to use the default Jest configuration. Additionally, the tsconfig.json file is updated to set the root directory to the project root.

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["tests/**/*.test.ts"],
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
   moduleNameMapper: {
     "^@interfaces/(.*)$": "<rootDir>/src/interfaces/$1",
     "^@controllers/(.*)$": "<rootDir>/src/controllers/$1",
